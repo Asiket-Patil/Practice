a2624ceddee9e342de52b2a0b1737792fe8684f2
Asiket-Patil <patilashiket6@gmail.com>
2025-10-29 19:28:39 +0530

    Updated Todo component to include status and action buttons

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Todo = ({ id, name, date, desc, status, onDelete, onDone }) => {
   return (
-    <div style={{ border: '1px solid black', padding: '10px', borderRadius: '10px', marginBottom: '10px' }}>
+    <div style={{ border: '1px solid black', padding: '10px', borderRadius: '10px', marginBottom: '10px' ,color:'black', backgroundColor:'white'}}>
       <h3>{name}</h3>
       <p><strong>Date:</strong> {date}</p>
       <p><strong>Description:</strong> {desc}</p>
